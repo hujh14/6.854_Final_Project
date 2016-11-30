bc21ec69e841f3fd79704ea719c0ac71fbe22e23
trattner <trattner@mit.edu>
2016-11-30 15:06:19 -0500

    graph class structure

---
+++ graph.py
@@ -0,0 +1,23 @@
+# Graph, edge, node classes for image segmentation
+# Andy Trattner and Jeffrey Hu
+# 11/30/16
+
+
+class Graph:
+    
+    
+    
+graph:
+    bunch of nodes (set list)
+    bunch of edges (set list)
+    
+node:
+    coordinates of pixel
+    color of pixel
+    intensity of pixel
+    list of neighbor nodes
+    source/sink
+    
+edge:
+    node1
+    node2
